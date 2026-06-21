c9b6f438e95357f9e832cce6b8ad8a7e7bbf0c49
Tri-Dang07 <pox5554@gmail.com>
2026-06-21 15:16:01 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-
+// Xử lý sự kiện khi người dùng bấm nút ở file external.html
+document.getElementById('btnExternal').addEventListener('click', function() {
+    alert('Bạn vừa bấm vào nút External JavaScript!');
+});
